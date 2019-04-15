ff5c7bffffa2cec0efe17685049b4394b3cc8e29
Danilo Lima <danilolima@wareline.com.br>
2019-04-15 10:42:50 -0300

    Add "Snapshoting..." message

--- src/public/app/src/containers/snapshotsPage/snapshotsPage.js
+++ src/public/app/src/containers/snapshotsPage/snapshotsPage.js
@@ -1,173 +1,176 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Line, Pie } from 'react-chartjs-2';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const dataPie = {
-  labels: [
-    'Serialized',
-    'Not Serialized',
-  ],
-  datasets: [{
-    data: [300, 50],
-    backgroundColor: [
-    '#FF6384',
-    '#36A2EB',
+    labels: [
+        'Serialized',
+        'Not Serialized',
     ],
-    hoverBackgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    ]
-  }]
+    datasets: [{
+        data: [300, 50],
+        backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+        ],
+        hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+        ]
+    }]
 };
 
 const data = {
-  labels: ['18 nov', '17nov', '16nov', '15nov', '14nov', '13nov', '12nov'],
-  datasets: [
-    {
-      label: 'serialized',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40],
+    labels: ['18 nov', '17nov', '16nov', '15nov', '14nov', '13nov', '12nov'],
+    datasets: [
+        {
+            label: 'serialized',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
 
-    },
-    {
-      label: 'not serialized',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'red',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'red',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [34, 22, 10, 12, 56, 55, 60],
+        },
+        {
+            label: 'not serialized',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'red',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'red',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [34, 22, 10, 12, 56, 55, 60],
 
-    }
-  ]
+        }
+    ]
 };
 
 class SnapshotsPage extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      path: "/placeholder",
-      snapshots: [],
-      isLoading: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            path: "/placeholder",
+            snapshots: [],
+            isLoading: false
+        }
     }
-  }
 
-  onPathChange = (ev) => {
-    this.setState({ path: ev.target.value });
-  }
+    onPathChange = (ev) => {
+        this.setState({ path: ev.target.value });
+    }
 
 
-  onClickSearch = () => {
-    this.search();
-  }
+    onClickTakeSnapshot = () => {
+        this.takeSnapshot();
+    }
 
-  fetchSnapshots = () => {
-    axios.get('http://localhost:4488/api/v1/snapshots')
-      .then(response => {
-        console.log(response);
-        if (response.data != null) {
-          this.setState({ snapshots: response.data })
+    fetchSnapshots = () => {
+        axios.get('http://localhost:4488/api/v1/snapshots')
+            .then(response => {
+                console.log(response);
+                if (response.data != null) {
+                    this.setState({ snapshots: response.data })
+                }
+            })
+    }
+
+    takeSnapshot = () => {
+        this.setState({ isLoading: true })
+        axios.get('http://localhost:4488/api/v1/parse?path=' + this.state.path)
+            .then(response => {
+                this.fetchSnapshots();
+            })
+            .finally(() => {
+                this.setState({ isLoading: false })
+            })
+    }
+
+    componentDidMount = () => {
+        this.fetchSnapshots();
+    }
+
+
+    render() {
+
+        if (this.state.isLoading) {
+            return (
+                <div>Snapshoting...</div>
+            )
         }
-      })
-  }
 
-  search = () => {
-    this.setState({ isLoading: true })
-    axios.get('http://localhost:4488/api/v1/parse?path=' + this.state.path)
-      .then(response => {
-        console.log(response);
-        if (response.data != null) {
-          this.setState({ files: response.data })
-        }
+        return (
+            <div className="container mt-5">
+                <div className="card">
+                    <div className="card-body p-5">
+                        <h4>Root folder: {this.state.path}</h4>
+                        <button onClick={this.onClickTakeSnapshot} className="btn btn-primary">Create Snapshot</button>
+                    </div>
 
-      })
-      .finally(() => {
-        this.setState({ isLoading: false })
-      })
-  }
+                </div>
 
-  componentDidMount = () => {
-    this.fetchSnapshots();
-  }
+                <div className="row">
+                    <div className="col-md-12">
+                        <Line data={data} height={100}></Line>
+                    </div>
+                </div>
 
+                <h3 className="mt-5"> Snapshots</h3>
+                <table className="table mb-5">
+                    <tbody>
+                        <tr>
+                            <th>Date</th>
+                            <th>Total Files</th>
+                            <th>Total Classes</th>
+                            <th>Serialized Classes</th>
+                            <th>Not Serialized Classes</th>
+                            <th>Auto Serialized</th>
+                            <th></th>
+                        </tr>
+                        {this.state.snapshots.map((snapshot, i) => {
+                            return <tr key={i}>
+                                <td>{snapshot.date}</td>
+                                <td>{snapshot.statistics['countFiles']}</td>
+                                <td>{snapshot.statistics['countClasses']}</td>
+                                <td>{snapshot.statistics['countClassesSerialized']}</td>
+                                <td>{snapshot.statistics['countClassesNotSerialized']}</td>
+                                <td>{snapshot.statistics['countReplacements']}</td>
+                                <td><Link to={"/snapshots/" + snapshot._id} className="btn btn-primary">View</Link></td>
+                            </tr>
+                        })}
+                    </tbody>
 
-  render() {
-    return (
-      <div className="container mt-5">
-        <div className="card">
-          <div className="card-body p-5">
-            <h4>Root folder: {this.state.path}</h4>
-            <button onClick={this.onClickSearch} className="btn btn-primary">Create Snapshot</button>
-          </div>
+                </table>
 
-        </div>
-
-        <div className="row">
-          <div className="col-md-12">
-            <Line data={data} height={100}></Line>
-          </div>
-        </div>
-
-        <h3 className="mt-5"> Snapshots</h3>
-        <table className="table mb-5">
-          <tbody>
-            <tr>
-              <th>Date</th>
-              <th>Total Files</th>
-              <th>Total Classes</th>
-              <th>Serialized Classes</th>
-              <th>Not Serialized Classes</th>
-              <th>Auto Serialized</th>
-              <th></th>
-            </tr>
-            {this.state.snapshots.map((snapshot, i) => {
-              return <tr key={i}>
-                <td>{snapshot.date}</td>
-                <td>{snapshot.statistics['countFiles']}</td>
-                <td>{snapshot.statistics['countClasses']}</td>
-                <td>{snapshot.statistics['countClassesSerialized']}</td>
-                <td>{snapshot.statistics['countClassesNotSerialized']}</td>
-                <td>{snapshot.statistics['countReplacements']}</td>
-                <td><Link to={"/snapshots/" + snapshot._id} className="btn btn-primary">View</Link></td>
-              </tr>
-            })}
-          </tbody>
-
-        </table>
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default SnapshotsPage;
